90967091c70409ba53cfad523992ccef2b0d4159
JaNi-Ha <jhlee94111@gmail.com>
2024-11-21 16:44:28 +0900

    Update 신사업팀.js

--- pages/news/신사업팀.js
+++ pages/news/신사업팀.js
@@ -1,1 +1,20 @@
+import Card from '../../components/Card';
+
+const CM2News = () => {
+  const newsData = [
+    { title: '신사업팀 뉴스 제목 1', content: '신사업팀 관련 뉴스 내용 1' },
+    { title: '신사업팀 뉴스 제목 2', content: '신사업팀 관련 뉴스 내용 2' },
+  ];
+
+  return (
+    <div>
+      <h1>CM2 뉴스</h1>
+      {newsData.map((news, index) => (
+        <Card key={index} title={news.title} content={news.content} />
+      ))}
+    </div>
+  );
+};
+
+export default CM2News;
 
