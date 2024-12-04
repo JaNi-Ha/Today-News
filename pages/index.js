86b7b3c5a1e2d14fba6c854dddd5734f73eb85d1
JaNi-Ha <jhlee94111@gmail.com>
2024-12-04 15:43:38 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -50,6 +50,7 @@ export default function Home() {
             content={post.내용}
             category={post.카테고리}
             imageUrl={post.이미지}
+            sourceUrl={post.출처}
           />
         ))}
       </Container>
